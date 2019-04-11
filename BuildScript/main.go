817f681d4a4692a5472127cccd7330303eec8fe8
Abhin P Jose <abhinjose@live.com>
2019-04-11 21:23:43 +0530

    Completed Build Script

--- BuildScript/main.go
+++ BuildScript/main.go
@@ -4,44 +4,127 @@ package main
 // must do pull, build and the tar the the binary and copy it to final dest
 // the use the compiled binary to build a docker image and push it docker.io
 import (
-	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
 var goPath = ""
 
 func main() {
+
+	log.Println("Build Started")
 	finalDest, err := os.Getwd()
-	printError("Error in getting Current Working Dir env variable", err)
-	fmt.Println(finalDest)
+	printError("Error in getting Current Working Dir: ", err)
 	goPath, isPresent := os.LookupEnv("GOPATH")
 	if !isPresent {
-		homeDir, _ := os.LookupEnv("HOME")
-		fmt.Println(homeDir)
+		homeDir, isPresent := os.LookupEnv("HOME")
+		if !isPresent {
+			log.Fatalln("$HOME env var not found")
+		}
 		goPath = homeDir + "/staging_dir/go"
-		fmt.Println("Go Path not set")
-		fmt.Println("Using \"" + goPath + "\" as default value for GOPATH")
+		log.Println("Go Path not set")
+		log.Println("Using `" + goPath + "` as default value for GOPATH")
 		err = os.Setenv("GOPATH", goPath)
 		printError("Error in setting GOPATH env variable", err)
 	}
-	workingDir := goPath + "/src"
-	err = os.RemoveAll(goPath)
-	printError("Error pre-cleaning up  Directory \""+goPath+"\"", err)
-	err = os.MkdirAll(workingDir, 0755)
-	printError("Error creating Directory \"src\"", err)
 
+	// Clean up before build
+	cleanUp(goPath)
+
+	// Clean up after build
+	defer cleanUp(goPath)
+
+	// Make src dir for go code and change to it.
+	log.Println("Start: creating /src dir and changing into it.")
+	workingDir := goPath + "/src"
+	err = os.MkdirAll(workingDir, 0755)
+	printError("Error creating Directory `src`", err)
 	err = os.Chdir(workingDir)
 	printError("Error changing working directory to  \""+workingDir+"\" : ", err)
-	str, _ := os.Getwd()
-	err = os.RemoveAll(goPath)
-	printError("Error pre-cleaning up  Directory \""+goPath+"\"", err)
-	fmt.Println(str)
-	fmt.Println(isPresent)
+	log.Println("End: creating /src dir and changing into it.")
+
+	// git clone the repo
+	log.Println("Start: git cloning")
+	repo := "https://github.com/Abhinkop/UrlShortnerGoLang.git"
+	cmd := exec.Command("git", "clone", repo)
+	err = cmd.Run()
+	printError("git clone failed with :", err)
+	log.Println("End: git cloning")
+
+	// change to UrlShortnerGoLang dir to build
+	log.Println("Start: change to `UrlShortnerGoLang` dir to build")
+	err = os.Chdir(workingDir + "/UrlShortnerGoLang")
+	printError("changing dir to `"+workingDir+"` UrlShortnerGoLang failed :", err)
+	log.Println("End: change to `UrlShortnerGoLang` dir to build")
+
+	// dep ensue to pull dependecies
+	log.Println("Start: fetching dependecies (dep ensure)")
+	cmd = exec.Command("dep", "ensure")
+	out, err := cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("`dep ensure` failed with :", err)
+	log.Println("End: fetching dependencies (dep ensur)e")
+
+	// Build main.go
+	log.Println("Start: go build main.go")
+	cmd = exec.Command("go", "build", "main.go")
+	out, err = cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("`go build maing.go` failed with :", err)
+	log.Println("End: go build main.go")
+
+	// create and copy the resouces and binary to build_output dir
+	log.Println("Start: copy `resources` to `build_output")
+	buildOutput := goPath + "/build_output"
+	err = os.MkdirAll(buildOutput, 0755)
+	printError("Error creating Directory :"+buildOutput, err)
+	cmd = exec.Command("cp", "-r", "resources", buildOutput)
+	out, err = cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("copying `resources` folder failed with :", err)
+	log.Println("Start: copy `main` to `build_output")
+	cmd = exec.Command("cp", "-r", "main", buildOutput)
+	out, err = cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("copying `main` binary failed with :", err)
+	log.Println("End: finished copying")
+
+	// change to $GOPATH dir to build tar
+	log.Println("Start: change dir to " + goPath)
+	err = os.Chdir(goPath)
+	printError("changing dir to "+goPath+"UrlShortnerGoLang failed :", err)
+	log.Println("End: change dir to " + goPath)
+
+	tarFileName := "Url.tar"
+	// tarball the build
+	log.Println("Start: Building tar")
+	cmd = exec.Command("tar", "-cf", tarFileName, "build_output")
+	out, err = cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("Building tar failed with :", err)
+	log.Println("End: Building tar")
+
+	// copy to final destination
+	log.Println("Start: Copy to " + finalDest)
+	cmd = exec.Command("cp", tarFileName, finalDest)
+	out, err = cmd.CombinedOutput()
+	log.Println("cmd Output :",out)
+	printError("copy to final destination("+finalDest+") failed with :", err)
+	log.Println("End: Copy to " + finalDest)
+
+	log.Println("Build Completed. Binaries copied to " + finalDest + "/"+tarFileName)
 }
 
 func printError(message string, err error) {
 	if err != nil {
-		fmt.Println(message+" : ", err)
-		os.Exit(-1)
+		log.Fatalln(message+" : ", err)
 	}
+}
+
+func cleanUp(goPath string) {
+	log.Println("Start: removing "+ goPath +" dir.")
+	err := os.RemoveAll(goPath)
+	printError("Error cleaning up  Directory \""+goPath+"\"", err)
+	log.Println("End: removing "+ goPath +" dir.")
 }
